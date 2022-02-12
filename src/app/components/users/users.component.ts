fd343f5428831890128ddc87cd33fe6d25d56d14
Yevhen <yevhenxsydorenko@gmail.com>
2022-02-13 00:29:41 +0200

    preps

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-
-@Component ({
-selector: 'app-users',
-templateUrl: './users.component.html'
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
 })
+export class UsersComponent implements OnInit {
 
+  constructor() { }
 
+  ngOnInit(): void {
+  }
 
-export class UsersComponent {
 }
