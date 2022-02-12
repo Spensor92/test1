6cd5ad5cfe9a9f929ee6cda300e1f68475362e0b
Yevhen <yevhenxsydorenko@gmail.com>
2022-02-12 20:44:48 +0200

    Components-users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {UsersComponent} from './components/users/users.component';
+import { UserListComponent } from './components/users/user-list/user-list/user-list.component';
+import { UserDetailsComponent } from './components/users/user-details/user-details/user-details.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserListComponent,
+    UserDetailsComponent
+
   ],
   imports: [
     BrowserModule,
