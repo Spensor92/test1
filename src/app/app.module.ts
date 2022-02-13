de4fa647e60aa3b8dfc1e01b6def3fe00f200cb4
Yevhen <yevhenxsydorenko@gmail.com>
2022-02-13 18:58:05 +0200

    redirectoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {UsersComponent} from './components/users/users.component';
-import { UserListComponent } from './components/users/user-list/user-list/user-list.component';
-import { UserDetailsComponent } from './components/users/user-details/user-details/user-details.component'
+import { UserListComponent } from './components/users/user-list/user-list.component';
+import { UserDetailsComponent } from './components/users/user-details/user-details.component'
 @NgModule({
   declarations: [
     AppComponent,
